a9cebb015674105b2650daa9b0daf23fcd62e50e
Ali Alsarraf <baaa555@outlook.com>
2025-02-16 18:33:49 +0300

    Part 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+
+    val kotlinMasterClass = Book("Kotlin guide from zero to hero", yearPublished = 2025, author = "Ali Taqi & Majed" );
+    println(kotlinMasterClass.getBookInfo())
+
+}
+
+
+
+class Book(var title: String, var author: String, var yearPublished: Int) {
+
+    fun getBookInfo():String{
+        return "$title By $author ($yearPublished)";
+    }
+
 }
